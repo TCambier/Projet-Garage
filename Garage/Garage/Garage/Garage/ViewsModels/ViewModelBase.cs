9fd5c66c585880732babbb9e7649cb1bdee9271e
Thomas Cambier <thomas.cambier1@gmail.com>
2026-03-05 16:23:32 +0100

    Delete Garage/Garage/Garage directory

--- Garage/Garage/Garage/Garage/ViewsModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-public class ViewModelBase : INotifyPropertyChanged
-{
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected void Raise([CallerMemberName] string name = null) =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-}
